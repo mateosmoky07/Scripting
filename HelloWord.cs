7fc864a32cca8443289d93c04dfacd2db95ec27e
mateosmoky07 <josipasmok333@gmail.com>
2024-12-15 12:11:24 +0100

    Add files via upload

---
+++ HelloWord.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWord : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Ovo je komentar
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
